a4e1ae399ea6f2a4767c7d0259532c4a482efaf2
nojapakirsteingenzen <nojapakirsteingenzen@gmail.com>
2026-03-14 04:23:36 +0800

    Update reviews.php

--- api/reviews.php
+++ api/reviews.php
@@ -1,1 +1,31 @@
+<?php
 
+header("Content-Type: application/json");
+
+$conn = new mysqli("localhost","DB_USER","DB_PASSWORD","launc43280_directory");
+
+if ($conn->connect_error) {
+    die(json_encode(["error"=>"DB connection failed"]));
+}
+
+$sql = "
+SELECT *
+FROM users_reviews
+WHERE review_status = 2
+ORDER BY review_added DESC
+LIMIT 200
+";
+
+$result = $conn->query($sql);
+
+$reviews = [];
+
+while($row = $result->fetch_assoc()){
+    $reviews[] = $row;
+}
+
+echo json_encode([
+    "reviews"=>$reviews
+]);
+
+?>
